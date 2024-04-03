041574bf2de43e0d5fde75586fee8ccc80a113c3
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-03 07:29:05 +0200

    Add test project

---
+++ src/tests/vm-start-stop-tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace vm_start_stop_tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

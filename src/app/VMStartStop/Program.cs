3e18d4ed19f46f3d5c013f09cec1ece955f24278
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-05 21:40:28 +0200

    Register TagManagerService

--- src/app/VMStartStop/Program.cs
+++ src/app/VMStartStop/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyCo.TagManager;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddTransient<ITagManagerService, TagManagerService>();
+    })
     .Build();
 
 host.Run();

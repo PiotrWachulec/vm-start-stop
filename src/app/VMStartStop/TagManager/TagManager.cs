b540bea24986fca82622d227460184ad29d42b6d
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-08 07:19:01 +0200

    Clean up time triggered Azure function

--- src/app/VMStartStop/TagManager/TagManager.cs
+++ src/app/VMStartStop/TagManager/TagManager.cs
@@ -1,8 +1,5 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
-using Azure.Identity;
-using Azure.ResourceManager;
-using System;
 
 namespace MyCo.TagManager
 {
@@ -17,13 +14,6 @@ namespace MyCo.TagManager
             _tagManagerService = tagManagerService;
         }
 
-        private bool IsCurrentTag(string tagValue)
-        {
-            // Check if the tag value is the current time to turn on or off
-
-            return false;
-        }
-
         [Function("TagManager")]
         public void Run([TimerTrigger("0 */15 * * * *")] TimerInfo myTimer)
         {
@@ -33,44 +23,6 @@ namespace MyCo.TagManager
             {
                 _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
             }
-
-            _tagManagerService.IsCurrentTag("tagValue");
-
-            // ArmClient client = new ArmClient(new DefaultAzureCredential());
-
-            // var subscriptions = client.GetSubscriptions();
-
-            // foreach (var subscription in subscriptions)
-            // {
-            //     _logger.LogInformation($"Subscription: {subscription.Data.SubscriptionId}");
-
-            //     var resourceGroups = subscription.GetResourceGroups();
-
-            //     foreach (var resourceGroup in resourceGroups)
-            //     {
-            //         _logger.LogInformation($"Resource Group: {resourceGroup.Data.Name}");
-
-            //         var resources = resourceGroup.GetResources();
-
-            //         foreach (var resource in resources)
-            //         {
-            //             _logger.LogInformation($"Resource: {resource.Data.Name}");
-
-            //             var tags = resource.GetTags();
-
-            //             foreach (var tag in tags)
-            //             {
-            //                 _logger.LogInformation($"Tag: {tag.Name}, Value: {tag.Value}");
-            //             }
-            //         }
-            //     }
-            // }
-            
-
-            // foreach (var tag in tags)
-            // {
-            //     _logger.LogInformation($"Tag: {tag.Name}, Value: {tag.Value}");
-            // }
         }
     }
 }

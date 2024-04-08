d363f38a2f6548b3c1558172410dc96b2a16e0e4
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-08 07:16:55 +0200

    Add VO for tag value

---
+++ src/app/VMStartStop/TagManager/VMStartStopTagValue.cs
@@ -0,0 +1,22 @@
+namespace MyCo.TagManager;
+
+public class VMStartStopTagValue
+{
+    public bool IsOn { get; }
+    public string StartTime { get; }
+    public string EndTime { get; }
+    public string TimeZone { get; }
+    public string Days { get; }
+
+    public VMStartStopTagValue(string tagValue)
+    {
+        string[] tagValues = tagValue.Split(";");
+        IsOn = tagValues[0].Equals("ON");
+        TimeZone = tagValues[2];
+        Days = tagValues[3];
+
+        var timeValues = tagValues[1].Split("-");
+        StartTime = timeValues[0];
+        EndTime = timeValues[1];
+    }
+}

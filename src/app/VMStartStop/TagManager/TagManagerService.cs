2a7cf45fe4dc6cbfb7be532dca5cdd0bfe7979e6
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-10 14:18:45 +0200

    Add some part of code to test

--- src/app/VMStartStop/TagManager/TagManagerService.cs
+++ src/app/VMStartStop/TagManager/TagManagerService.cs
@@ -25,11 +25,33 @@ namespace MyCo.TagManager
             throw new NotImplementedException();
         }
 
-        public void GetTagsFromAzure()
+        public async Task GetTagsFromAzure()
         {
+            // 1. Get all subscriptions
+            // 2. Check if the subscription has the VMStartStop tag
+            // - If yes, then switch all VMs on subscription
+            // - If no, then:
+            // 3. Get all resource groups
+            // 4. Check if the resource group has the VMStartStop tag
+            // - If yes, then switch all VMs on resource group
+            // - If no, then:
+            // 5. Get all VMs
+            // 6. Check if the VM has the VMStartStop tag and switch them if needed
+
             ArmClient armClient = new ArmClient(new DefaultAzureCredential());
 
             var subscriptions = armClient.GetSubscriptions();
+
+            var count = subscriptions.Count();
+
+            foreach (var sub in subscriptions)
+            {
+
+                var resources = await sub.GetGenericResourcesAsync();
+                sub.Data.Tags.ContainsKey("VMStartStop");
+            }
+
+            _logger.LogInformation($"Found {count} subscriptions");
         }
     }
 }

bf4d94daf74bcc1246b3f007d7d8ab3718ffb3d0
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-13 15:46:35 +0200

    Add new method in TagsRepository for getting tags from RGs

--- src/app/VMStartStop/TagManager/TagsRepository.cs
+++ src/app/VMStartStop/TagManager/TagsRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using Azure.Identity;
 using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
 using Microsoft.Extensions.Logging;
 
 namespace MyCo.TagManager;
@@ -16,7 +17,40 @@ public class TagsRepository : ITagsRepository
         _armClient = new ArmClient(new DefaultAzureCredential());
     }
 
-    public async Task<IEnumerable<Subscription>> GetTagsFromSubscriptions()
+    public IEnumerable<ResourceGroup> GetTagsFromResourceGroups()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<ResourceGroup> GetTagsFromResourceGroupsInSubscription(SubscriptionResource subscription)
+    {
+        var resourceGroups = subscription.GetResourceGroups();
+        
+        Collection<ResourceGroup> resourceGroupWithTags = new Collection<ResourceGroup>();
+
+        foreach (var resourceGroup in resourceGroups)
+        {
+            if (resourceGroup.Data.Tags.ContainsKey("VM-START-STOP-SCHEDULE"))
+            {
+                _logger.LogInformation($"Resource group {resourceGroup.Data.Id} has VMStartStop tag");
+                resourceGroupWithTags.Add(
+                    new ResourceGroup(
+                        resourceGroup.Data.Name,
+                        new SubscriptionId(subscription.Data.Id),
+                        new VMStartStopTagValue(resourceGroup.Data.Tags["VM-START-STOP-SCHEDULE"])
+                    )
+                );
+            }
+            else
+            {
+                _logger.LogInformation($"Resource group {resourceGroup.Data.Id} does not have VMStartStop tag");
+            }
+        }
+
+        return resourceGroupWithTags;
+    }
+
+    public IEnumerable<Subscription> GetTagsFromSubscriptions()
     {
         var subscriptions = _armClient.GetSubscriptions();
 

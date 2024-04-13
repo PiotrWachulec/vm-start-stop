a5b726ba9c209d72b59b280c245fccabb8ec324d
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-13 15:24:30 +0200

    Add class for ResourceGroup

---
+++ src/app/VMStartStop/TagManager/ResourceGroup.cs
@@ -0,0 +1,15 @@
+namespace MyCo.TagManager;
+
+public class ResourceGroup
+{
+    public string Name { get; }
+    public SubscriptionId SubscriptionId { get; }
+    public VMStartStopTagValue TagValue { get; }
+
+    public ResourceGroup(string name, SubscriptionId subscriptionId, VMStartStopTagValue tagValue)
+    {
+        Name = name;
+        SubscriptionId = subscriptionId;
+        TagValue = tagValue;
+    }
+}

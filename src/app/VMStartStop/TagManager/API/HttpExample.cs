69b849c93ef4ad2a9ee384f2d6fcbafa7aa62402
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-16 05:58:55 +0200

    Add HttpExample and TagManager function to API layer

--- src/app/VMStartStop/TagManager/API/HttpExample.cs
+++ src/app/VMStartStop/TagManager/API/HttpExample.cs
@@ -2,22 +2,28 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using MyCo.TagManager;
 
 namespace app
 {
     public class HttpExample
     {
         private readonly ILogger _logger;
+        private readonly ITagManagerService _tagManagerService;
 
-        public HttpExample(ILoggerFactory loggerFactory)
+        public HttpExample(ILoggerFactory loggerFactory,
+            ITagManagerService tagManagerService)
         {
             _logger = loggerFactory.CreateLogger<HttpExample>();
+            _tagManagerService = tagManagerService;
         }
 
         [Function("HttpExample")]
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            _tagManagerService.GetTagsFromAzure();
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");

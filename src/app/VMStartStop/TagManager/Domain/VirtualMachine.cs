41ff5ceef7c07e792f5e6c722d75537f2c2d8b01
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-16 06:09:21 +0200

    Update NS in VirtualMachine

--- src/app/VMStartStop/TagManager/Domain/VirtualMachine.cs
+++ src/app/VMStartStop/TagManager/Domain/VirtualMachine.cs
@@ -1,16 +1,15 @@
-namespace MyCo.TagManager
-{
-    public class VirtualMachine
-    {
-        public string Name { get; }
-        public SubscriptionId SubscriptionId { get; }
-        public VMStartStopTagValue TagValue { get; }
+namespace MyCo.TagManager.Domain;
 
-        public VirtualMachine(string name, SubscriptionId subscriptionId, VMStartStopTagValue tagValue)
-        {
-            Name = name;
-            SubscriptionId = subscriptionId;
-            TagValue = tagValue;
-        }
+public class VirtualMachine
+{
+    public string Name { get; }
+    public SubscriptionId SubscriptionId { get; }
+    public VMStartStopTagValue TagValue { get; }
+
+    public VirtualMachine(string name, SubscriptionId subscriptionId, VMStartStopTagValue tagValue)
+    {
+        Name = name;
+        SubscriptionId = subscriptionId;
+        TagValue = tagValue;
     }
 }

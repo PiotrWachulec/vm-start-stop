1ca655e5eb157b58418261920be0f535595a8f2d
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-16 06:18:31 +0200

    Update NS in HTTPExample

--- src/app/VMStartStop/TagManager/API/HttpExample.cs
+++ src/app/VMStartStop/TagManager/API/HttpExample.cs
@@ -2,35 +2,34 @@ using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using MyCo.TagManager;
+using MyCo.TagManager.Application;
 
-namespace app
+namespace MyCo.TagManager.API;
+
+public class HttpExample
 {
-    public class HttpExample
+    private readonly ILogger _logger;
+    private readonly ITagManagerService _tagManagerService;
+
+    public HttpExample(ILoggerFactory loggerFactory,
+        ITagManagerService tagManagerService)
     {
-        private readonly ILogger _logger;
-        private readonly ITagManagerService _tagManagerService;
+        _logger = loggerFactory.CreateLogger<HttpExample>();
+        _tagManagerService = tagManagerService;
+    }
 
-        public HttpExample(ILoggerFactory loggerFactory,
-            ITagManagerService tagManagerService)
-        {
-            _logger = loggerFactory.CreateLogger<HttpExample>();
-            _tagManagerService = tagManagerService;
-        }
+    [Function("HttpExample")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        [Function("HttpExample")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+        _tagManagerService.GetTagsFromAzure();
 
-            _tagManagerService.GetTagsFromAzure();
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        response.WriteString("VM Start Stop: Welcome to Azure Functions!");
 
-            response.WriteString("VM Start Stop: Welcome to Azure Functions!");
-
-            return response;
-        }
+        return response;
     }
 }

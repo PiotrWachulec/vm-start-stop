bcc30f863e240cf255ccc8b6dedb4df9555b5abc
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-16 06:17:50 +0200

    Update NS in TagManager

--- src/app/VMStartStop/TagManager/API/TagManager.cs
+++ src/app/VMStartStop/TagManager/API/TagManager.cs
@@ -1,28 +1,29 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using MyCo.TagManager.Application;
 
-namespace MyCo.TagManager
+namespace MyCo.TagManager.API;
+
+public class TagManager
 {
-    public class TagManager
+    private readonly ILogger _logger;
+    private readonly ITagManagerService _tagManagerService;
+
+    public 
+    TagManager(ILoggerFactory loggerFactory, ITagManagerService tagManagerService)
     {
-        private readonly ILogger _logger;
-        private readonly ITagManagerService _tagManagerService;
+        _logger = loggerFactory.CreateLogger<TagManager>();
+        _tagManagerService = tagManagerService;
+    }
 
-        public TagManager(ILoggerFactory loggerFactory, ITagManagerService tagManagerService)
+    [Function("TagManager")]
+    public void Run([TimerTrigger("0 */15 * * * *")] TimerInfo myTimer)
+    {
+        _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+        if (myTimer.ScheduleStatus is not null)
         {
-            _logger = loggerFactory.CreateLogger<TagManager>();
-            _tagManagerService = tagManagerService;
-        }
-
-        [Function("TagManager")]
-        public void Run([TimerTrigger("0 */15 * * * *")] TimerInfo myTimer)
-        {
-            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-
-            if (myTimer.ScheduleStatus is not null)
-            {
-                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
-            }
+            _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
         }
     }
 }

bac50c95d8260f4f244a5b32ca68b07b710a8fbf
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-17 07:51:01 +0200

    Add command for tags processing

---
+++ src/app/VMStartStop/TagManager/Application/Commands/Handlers/ProcessTagsHandler.cs
@@ -0,0 +1,24 @@
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace MyCo.TagManager.Application.Commands.Handlers;
+
+public class ProcessTagsHandler : IRequestHandler<ProcessTags>
+{
+    private readonly ITagManagerService _tagManagerService;
+    private readonly ILogger _logger;
+
+    public ProcessTagsHandler(ITagManagerService tagManagerService, ILoggerFactory loggerFactory)
+    {
+        _tagManagerService = tagManagerService;
+        _logger = loggerFactory.CreateLogger<ProcessTagsHandler>();
+    }
+
+    public Task Handle(ProcessTags request, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Processing tags");
+        _tagManagerService.GetTagsFromAzure();
+        return Task.CompletedTask;
+    }
+}
---
+++ src/app/VMStartStop/TagManager/Application/Commands/ProcessTags.cs
@@ -0,0 +1,8 @@
+using MediatR;
+
+namespace MyCo.TagManager.Application.Commands;
+
+public class ProcessTags : IRequest
+{
+
+}

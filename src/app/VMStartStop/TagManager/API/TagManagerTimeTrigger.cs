95cd9ed7f9922f3cf4e418e1f7e3e87de105a87d
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-18 07:24:18 +0200

    Add tag processing based on time trigger

--- src/app/VMStartStop/TagManager/API/TagManagerTimeTrigger.cs
+++ src/app/VMStartStop/TagManager/API/TagManagerTimeTrigger.cs
@@ -1,6 +1,8 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using MyCo.TagManager.Application;
+using MediatR;
+using MyCo.TagManager.Application.Commands;
 
 namespace MyCo.TagManager.API;
 
@@ -8,18 +10,26 @@ public class TagManagerTimeTrigger
 {
     private readonly ILogger _logger;
     private readonly ITagManagerService _tagManagerService;
+    
+    private readonly IMediator _mediator;
 
     public 
-    TagManagerTimeTrigger(ILoggerFactory loggerFactory, ITagManagerService tagManagerService)
+    TagManagerTimeTrigger(ILoggerFactory loggerFactory, ITagManagerService tagManagerService,
+        IMediator mediator)
     {
         _logger = loggerFactory.CreateLogger<TagManagerTimeTrigger>();
         _tagManagerService = tagManagerService;
+        _mediator = mediator;
     }
 
     [Function("TagManager")]
     public void Run([TimerTrigger("0 */15 * * * *")] TimerInfo myTimer)
     {
         _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+        _logger.LogInformation("Processing tags");
+        _mediator.Send(new ProcessTags());
+        _logger.LogInformation("Tags processed");
 
         if (myTimer.ScheduleStatus is not null)
         {

023e535153c44857ad9f08b87762e5c057a61295
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-22 19:41:56 +0200

    Add template for basic service bus triggered function

---
+++ src/app/VMStartStop/TagManager/API/ServiceBusQueueTrigger1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace MyCo.TagManager
+{
+    public class ServiceBusQueueTrigger1
+    {
+        private readonly ILogger<ServiceBusQueueTrigger1> _logger;
+
+        public ServiceBusQueueTrigger1(ILogger<ServiceBusQueueTrigger1> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(ServiceBusQueueTrigger1))]
+        public async Task Run(
+            [ServiceBusTrigger("time-trigger-service-bus-queue", Connection = "ServiceBusConnection")]
+            ServiceBusReceivedMessage message,
+            ServiceBusMessageActions messageActions)
+        {
+            _logger.LogInformation("Message ID: {id}", message.MessageId);
+            _logger.LogInformation("Message Body: {body}", message.Body);
+            _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+
+            // Complete the message
+            await messageActions.CompleteMessageAsync(message);
+        }
+    }
+}

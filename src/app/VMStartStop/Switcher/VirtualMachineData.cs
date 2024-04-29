febc89855fff85c6bdfc393afb7f0c1e4cbde3c0
Piotr Wachulec <p.wachulec@gmail.com>
2024-04-29 20:38:45 +0200

    Add VM Data object for message deserialization

---
+++ src/app/VMStartStop/Switcher/VirtualMachineData.cs
@@ -0,0 +1,8 @@
+namespace MyCo.Switcher;
+
+public class VirtualMachineData
+{
+    public string SubscriptionId { get; set; }
+    public string ResourceGroupName { get; set; }
+    public string VirtualMachineName { get; set; }
+}

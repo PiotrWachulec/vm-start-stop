d958fcab3e49c46f1d71809676c2c2bdade9a8b8
Piotr Wachulec <p.wachulec@gmail.com>
2024-05-03 17:23:38 +0200

    Add action parameter in message object

--- src/app/VMStartStop/Switcher/VirtualMachineData.cs
+++ src/app/VMStartStop/Switcher/VirtualMachineData.cs
@@ -1,8 +1,9 @@
 namespace MyCo.Switcher;
 
-public class VirtualMachineData(string subscriptionId, string resourceGroupName, string virtualMachineName)
+public class VirtualMachineData(string subscriptionId, string resourceGroupName, string virtualMachineName, string action)
 {
     public string SubscriptionId { get; } = subscriptionId;
     public string ResourceGroupName { get; } = resourceGroupName;
     public string VirtualMachineName { get; } = virtualMachineName;
+    public string Action { get; } = action;
 }

ee4ec60cb034d38acd7662c9066759935abaac94
Piotr Wachulec <p.wachulec@gmail.com>
2024-05-04 12:02:56 +0200

    Rename class for RG data

--- src/app/VMStartStop/Switcher/ResourceGroupToSwitchData.cs
+++ src/app/VMStartStop/Switcher/ResourceGroupToSwitchData.cs
@@ -1,6 +1,6 @@
 namespace MyCo.Switcher;
 
-public class ResourceGroupData
+public class ResourceGroupToSwitchData
 {
     public required string ResourceGroupName { get; init; }
     public required string Action { get; init; }

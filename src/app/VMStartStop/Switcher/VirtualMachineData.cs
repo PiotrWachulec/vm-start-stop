033eedfe1b4eb5f249409f632a8d6d60e4eeb618
Piotr Wachulec <p.wachulec@gmail.com>
2024-05-04 11:54:54 +0200

    Add required and init props in VM Data class

--- src/app/VMStartStop/Switcher/VirtualMachineData.cs
+++ src/app/VMStartStop/Switcher/VirtualMachineData.cs
@@ -2,8 +2,8 @@ namespace MyCo.Switcher;
 
 public class VirtualMachineData(string subscriptionId, string resourceGroupName, string virtualMachineName, string action)
 {
-    public string SubscriptionId { get; } = subscriptionId;
-    public string ResourceGroupName { get; } = resourceGroupName;
-    public string VirtualMachineName { get; } = virtualMachineName;
-    public string Action { get; } = action;
+    public required string SubscriptionId { get; init; } = subscriptionId;
+    public required string ResourceGroupName { get; init; } = resourceGroupName;
+    public required string VirtualMachineName { get; init; } = virtualMachineName;
+    public required string Action { get; init; } = action;
 }

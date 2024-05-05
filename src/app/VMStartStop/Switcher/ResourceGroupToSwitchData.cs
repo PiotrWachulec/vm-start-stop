1bb29a68eb7c55adcc70cfbbd0003d19f7e9dedf
Piotr Wachulec <p.wachulec@gmail.com>
2024-05-05 19:01:58 +0200

    Add subscription Id to resource group data class

--- src/app/VMStartStop/Switcher/ResourceGroupToSwitchData.cs
+++ src/app/VMStartStop/Switcher/ResourceGroupToSwitchData.cs
@@ -2,6 +2,7 @@ namespace MyCo.Switcher;
 
 public class ResourceGroupToSwitchData
 {
+    public required string SubscriptionId { get; init; }
     public required string ResourceGroupName { get; init; }
     public required string Action { get; init; }
 }

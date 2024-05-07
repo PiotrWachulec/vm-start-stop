d4fbec8833614d13eda6470b8837b8046fc1857a
Piotr Wachulec <p.wachulec@gmail.com>
2024-05-07 10:36:55 +0200

    Add checks in ResourceGroup data object init

--- src/app/VMStartStop/Switcher/ResourceGroupToSwitchData.cs
+++ src/app/VMStartStop/Switcher/ResourceGroupToSwitchData.cs
@@ -2,7 +2,52 @@ namespace MyCo.Switcher;
 
 public class ResourceGroupToSwitchData
 {
-    public required string SubscriptionId { get; init; }
-    public required string ResourceGroupName { get; init; }
-    public required string Action { get; init; }
+    public required string SubscriptionId
+    {
+        get
+        {
+            return SubscriptionId;
+        }
+        init
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("SubscriptionId is required", nameof(SubscriptionId));
+            }
+        }
+    }
+
+    public required string ResourceGroupName
+    {
+        get
+        {
+            return ResourceGroupName;
+        }
+        init
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("ResourceGroupName is required", nameof(ResourceGroupName));
+            }
+        }
+    }
+
+    public required string Action
+    {
+        get
+        {
+            return Action;
+        }
+        init
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Action is required", nameof(Action));
+            }
+            if (value != "start" && value != "stop")
+            {
+                throw new ArgumentException("Action must be either 'start' or 'stop'", nameof(Action));
+            }
+        }
+    }
 }

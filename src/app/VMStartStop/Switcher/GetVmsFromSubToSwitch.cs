d379e4d229cb53b43bb7804927a03bd16009fd1f
Piotr Wachulec <p.wachulec@gmail.com>
2024-05-10 17:44:40 +0200

    Fill function for switching VMs in subscription

--- src/app/VMStartStop/Switcher/GetVmsFromSubToSwitch.cs
+++ src/app/VMStartStop/Switcher/GetVmsFromSubToSwitch.cs
@@ -1,33 +1,86 @@
 using System;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
 using Azure.Messaging.ServiceBus;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Compute;
+using Azure.ResourceManager.Resources;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using MyCo.Switcher;
 
 namespace MyCo
 {
     public class GetVmsFromSubToSwitch
     {
+        private readonly ArmClient _armClient;
         private readonly ILogger<GetVmsFromSubToSwitch> _logger;
 
         public GetVmsFromSubToSwitch(ILogger<GetVmsFromSubToSwitch> logger)
         {
             _logger = logger;
+            _armClient = new ArmClient(new DefaultAzureCredential());
         }
 
         [Function(nameof(GetVmsFromSubToSwitch))]
         [ServiceBusOutput("turn-on-off-vm-service-bus-queue", Connection = "WriteServiceBusConnection")]
-        public async Task Run(
+        public async Task<ICollection<VirtualMachineToSwitchData>> Run(
             [ServiceBusTrigger("switch-vm-in-sub-service-bus-queue", Connection = "ReadServiceBusConnection")]
             ServiceBusReceivedMessage message,
             ServiceBusMessageActions messageActions)
         {
             _logger.LogInformation("Message ID: {id}", message.MessageId);
-            _logger.LogInformation("Message Body: {body}", message.Body);
-            _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
 
-            // Complete the message
-            await messageActions.CompleteMessageAsync(message);
+            if (message.Body == null)
+            {
+                throw new ArgumentException("Message body is null", nameof(message));
+            }
+
+            try
+            {
+                JsonSerializerOptions options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                SubscriptionToSwitchData? resourceGroupData = JsonSerializer.Deserialize<SubscriptionToSwitchData>(
+                    message.Body.ToString(), options);
+
+                _logger.LogInformation("Switching VMs in subscription: {subId}", resourceGroupData?.SubscriptionId);
+
+                var subscription = _armClient.GetSubscriptionResource(
+                    SubscriptionResource.CreateResourceIdentifier(
+                        new ResourceIdentifier(resourceGroupData?.SubscriptionId)));
+
+                var vmsToSwitch = new List<VirtualMachineToSwitchData>();
+
+                await foreach (VirtualMachineResource vm in subscription.GetVirtualMachinesAsync())
+                {
+                    vmsToSwitch.Add(new()
+                    {
+                        SubscriptionId = resourceGroupData.SubscriptionId,
+                        ResourceGroupName = vm.Data.Id.ResourceGroupName,
+                        VirtualMachineName = vm.Data.Name,
+                        Action = resourceGroupData.Action
+                    });
+                }
+
+                return vmsToSwitch;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+
+                await messageActions.DeadLetterMessageAsync(message);
+
+                return [];
+            }
+            finally
+            {
+                await messageActions.CompleteMessageAsync(message);
+            }
         }
     }
 }

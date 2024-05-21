be92956cd0103dc8e8781570aa956c4bf1c72f08
Piotr Wachulec <p.wachulec@gmail.com>
2024-05-21 13:33:35 +0200

    Add trigger time to process tags command

--- src/app/VMStartStop/TagManager/Application/Commands/ProcessTags.cs
+++ src/app/VMStartStop/TagManager/Application/Commands/ProcessTags.cs
@@ -2,7 +2,7 @@ using MediatR;
 
 namespace MyCo.TagManager.Application.Commands;
 
-public class ProcessTags : IRequest
+public class ProcessTags(TimeOnly triggerTime) : IRequest
 {
-
+    public TimeOnly TriggerTime { get; } = triggerTime;
 }

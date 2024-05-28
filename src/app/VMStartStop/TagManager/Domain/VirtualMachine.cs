0ff89450032ec28cc258134f4551f0158954c549
Piotr Wachulec <p.wachulec@gmail.com>
2024-05-28 17:39:41 +0200

    Add resource group name into VM object

--- src/app/VMStartStop/TagManager/Domain/VirtualMachine.cs
+++ src/app/VMStartStop/TagManager/Domain/VirtualMachine.cs
@@ -4,12 +4,14 @@ public class VirtualMachine
 {
     public string Name { get; }
     public SubscriptionId SubscriptionId { get; }
+    public string ResourceGroupName { get; }
     public VMStartStopTagValue TagValue { get; }
 
-    public VirtualMachine(string name, SubscriptionId subscriptionId, VMStartStopTagValue tagValue)
+    public VirtualMachine(string name, SubscriptionId subscriptionId, string resourceGroupName, VMStartStopTagValue tagValue)
     {
         Name = name;
         SubscriptionId = subscriptionId;
+        ResourceGroupName = resourceGroupName;
         TagValue = tagValue;
     }
 }
